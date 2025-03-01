eba9d2bf00de9463dd46f1433617fdca7f265447
jahongirfarruxov <jahongirfarruxov.com>
2025-03-01 15:27:11 +0500

    1-dars

---
+++ 1-dars/script.js
@@ -0,0 +1,10 @@
+// Console bilan ishlash
+
+		 console.log("Salom, JavaScipt!");  // console.log= consolega xabar yuborish
+		console.error("Xatolik"); // console.error= consolega xatolik chiqarish  
+		console.warn("Ogohlantirish"); // console.warn= Consolega oldindan ogohkantirish
+
+		// Alert bilan ishlash
+		// alert("Salom JavaScipt");
+		// window.confirm("Rostmi?");
+		// window.prompt("Ismingizni kiriting");
